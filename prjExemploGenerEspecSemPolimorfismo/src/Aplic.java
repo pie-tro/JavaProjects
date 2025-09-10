d6e2fe01eb5ace8ff901ab057ce6d1759a235454
pie.tro <pietromaia28@gmail.com>
2025-09-10 10:04:23 -0300

    fixed professor.java

--- prjExemploGenerEspecSemPolimorfismo/src/Aplic.java
+++ prjExemploGenerEspecSemPolimorfismo/src/Aplic.java
@@ -1,4 +1,5 @@
 import fatec.poo.model.Aluno;
+import fatec.poo.model.Professor;
 /**
  *
  * @author tigri
@@ -7,10 +8,18 @@ public class Aplic {
     public static void main(String[] args) {
        Aluno objAlu = new Aluno(1010,"Carlos Silveira","15/03/1978");
        
-       objAlu.setMensalidade(1500);
+        objAlu.setMensalidade(1500);
         System.out.println("Registo escolar: "+ objAlu.getRegEscolar());
         System.out.println("Nome: "+ objAlu.getNome());
         System.out.println("Data nascimento: "+ objAlu.getDataNascimento());
         System.out.println("Mensalidade: "+ objAlu.getMensalidade());
+        System.out.println("\n\n\n");
+        Professor objProf = new Professor(2020, "Maria Oliveira", "20/08/1980");
+        
+        objProf.setSalario(7500);
+        System.out.println("Registro funcional: " + objProf.getRegFuncional());
+        System.out.println("Nome: " + objProf.getNome());
+        System.out.println("Data nascimento: " + objProf.getDataNascimento());
+        System.out.println("Salário: " + objProf.getSalario());
     }
 }
--- prjExemploGenerEspecSemPolimorfismo/src/fatec/poo/model/Professor.java
+++ prjExemploGenerEspecSemPolimorfismo/src/fatec/poo/model/Professor.java
@@ -4,9 +4,24 @@ package fatec.poo.model;
  *
  * @author tigri
  */
-public class Professor {
+public class Professor extends Pessoa {
     private int regFuncional;
     private double salario;
-    
-    public Professor(int )
+
+    public Professor(int rf, String n, String dn){
+        super(n, dn);
+        regFuncional = rf;
+    }
+
+    public void setSalario(double s){
+        salario = s;
+    }
+
+    public int getRegFuncional(){
+        return regFuncional;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
 }
